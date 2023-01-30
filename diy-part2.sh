8662f95623419b05cc54fbe9adc56e90769dd4df
lawkim-cn <62065177+lawkim-cn@users.noreply.github.com>
2023-01-30 09:10:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,11 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.252/g' package/base-files/files/bin/config_generate
+# Modify default Themes
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/argon/g' feeds/luci/collections/luci/Makefile
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+cd feeds/luci/themes
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
